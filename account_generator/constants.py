048edb0b9df61fee70c85497ee216bff2cb50b36
Kirbyy <81638158+Kirbyy1@users.noreply.github.com>
2023-09-11 15:44:19 +0300

    Create constants.py

---
+++ account_generator/constants.py
@@ -0,0 +1,63 @@
+API = "https://signup-api.leagueoflegends.com/v1/accounts"
+WEBSITE_KEY = "a010c060-9eb5-498c-a7b9-9204c881f9dc"
+
+CONFIG_URL = 'https://signup-api.leagueoflegends.com/v1/config'
+
+URLS = {
+    'EUW': 'https://signup.leagueoflegends.com/en-gb/signup/index#/',
+    'EUNE': 'https://signup.leagueoflegends.com/en-pl/signup/index#/',
+    'NA': 'https://signup.leagueoflegends.com/en-us/signup/index#/',
+    'BR': 'https://signup.leagueoflegends.com/pt-br/signup/index#/',
+    'TR': 'https://signup.leagueoflegends.com/tr-tr/signup/index#/',
+    'RU': 'https://signup.leagueoflegends.com/ru-ru/signup/index#/',
+    'OCE': 'https://signup.leagueoflegends.com/en-au/signup/index#/',
+    'LAN': 'https://signup.leagueoflegends.com/es-mx/signup/index#/',
+    'LAS': 'https://signup.leagueoflegends.com/es-ar/signup/index#/',
+    'JP': 'https://signup.leagueoflegends.com/ja-jp/signup/index#/',
+    'PH': 'https://signup.leagueoflegends.com/en-ph/signup/index#/',
+    'SG': 'https://signup.leagueoflegends.com/en-sg/signup/index#/',
+    'TH': 'https://signup.leagueoflegends.com/th-th/signup/index#/',
+    'TW': 'https://signup.leagueoflegends.com/zh-tw/signup/index#/',
+    'VN': 'https://lienminh.vnggames.com/dang-ky/#/',
+    'PBE': 'https://signup.leagueoflegends.com/en-be/signup/index#/',
+    'default': 'https://auth.riotgames.com/login'
+}
+
+REGIONS = {
+  'EUW': 'EUW1',
+  'EUNE': 'EUN1',
+  'NA': 'NA1',
+  'BR': 'BR1',
+  'TR': 'TR1',
+  'RU': 'RU',
+  'OCE': 'OC1',
+  'LAN': 'LA1',
+  'LAS': 'LA2',
+  'JP': 'JP1',
+  'PH': 'PH2',
+  'SG': 'SG2',
+  'TH': 'TH2',
+  'TW': 'TW2',
+  'VN': None,
+  'PBE': 'PBE1'
+}
+
+
+LOCALES = {
+  'EUW': 'en',
+  'EUNE': 'en',
+  'NA': 'en',
+  'BR': 'pt',
+  'TR': 'tr',
+  'RU': 'ru',
+  'OCE': 'en',
+  'LAN': 'es',
+  'LAS': 'es',
+  'JP': 'ja',
+  'PH': 'en',
+  'SG': 'en',
+  'TH': 'th',
+  'TW': 'zh-Hant',
+  'VN': 'vi',
+  'PBE': 'en'
+}
