a4d1e19c6b8b325056ff20921eb902d2929611da
Kirbyy <81638158+Kirbyy1@users.noreply.github.com>
2023-09-14 02:34:30 +0300

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -4,89 +4,15 @@ import string
 import sys
 import threading
 import tkinter as tk
-from tkinter import filedialog
-from tkinter import messagebox
-from tkinter import ttk
+from ctypes import windll
 
+import customtkinter
+from CTkMessagebox import CTkMessagebox
 from PIL import ImageTk, Image
-from tkextrafont import Font
 
 from account_generator import GenerateAccount
 
-# Define the regions
-REGIONS = {
-    'EUW': 'EUW1',
-    'EUNE': 'EUN1',
-    'NA': 'NA1',
-    'BR': 'BR1',
-    'TR': 'TR1',
-    'RU': 'RU',
-    'OCE': 'OC1',
-    'LAN': 'LA1',
-    'LAS': 'LA2',
-    'JP': 'JP1',
-    'PH': 'PH2',
-    'SG': 'SG2',
-    'TH': 'TH2',
-    'TW': 'TW2',
-    'VN': None,
-    'PBE': 'PBE1'
-}
 
-
-def generate_accounts_in_thread():
-    num_accounts = num_accounts_entry.get()
-
-    if num_accounts.isdigit() and int(num_accounts) > 0:
-        thread = threading.Thread(target=generate_account)
-        thread.start()
-    else:
-        messagebox.showerror("Invalid Input", "Please enter a valid positive integer for the number of accounts.")
-
-
-def generate_account():
-    num_accounts = num_accounts_entry.get()
-
-    if not num_accounts.isdigit() or int(num_accounts) <= 0:
-        messagebox.showerror("Invalid Input", "Please enter a valid positive integer for the number of accounts.")
-        return
-
-    num_accounts = int(num_accounts)
-
-    try:
-        account_generator = GenerateAccount()
-        account_generator.CAPMONSTER_KEY = api_key_entry.get()  # Set the CAPMONSTER_KEY
-        for _ in range(num_accounts):
-            password = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
-            username = ''.join(random.choices(string.ascii_lowercase, k=8))
-            region = region_combobox.get()
-            response = account_generator.generate_account(password, username, region)
-            account_treeview.insert('', tk.END, values=(username, password))
-        messagebox.showinfo("Accounts Generated", f"{num_accounts} accounts generated successfully!")
-    except Exception as e:
-        messagebox.showerror("Error", f"An error occurred: {str(e)}")
-
-
-def save_accounts():
-    items = account_treeview.get_children()
-    accounts = []
-    for item in items:
-        values = account_treeview.item(item, 'values')
-        accounts.append(f"{values[0]}:{values[1]}")
-    accounts_text = '\n'.join(accounts)
-
-    # Open a file dialog to choose the file location
-    file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text Files", "*.txt")])
-
-    if file_path:
-        # Save the accounts text to the chosen file
-        with open(file_path, "w") as file:
-            file.write(accounts_text)
-        messagebox.showinfo("Accounts Saved", "Accounts saved successfully!")
-
-
-# This function can get temp path for your resource file
-# relative_path is your icon file name
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     try:
@@ -98,92 +24,289 @@ def resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 
-# Create the window
-window = tk.Tk()
-window.title("Account Generator")
-window.geometry("500x800")
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
 
-# Set the window icon
-window.iconbitmap(resource_path("assets/rglogo.ico"))
+        # Setting application variables:
+        self.window_title = 'Riot Generator v1.0'  # Change here title of your app
 
-# Load the image
-image_path = resource_path("assets/rglogo.png")
-image = Image.open(image_path)
+        self.put_icon = True
 
-# Resize the image
-width, height = 170, 170  # Set the desired width and height
-image = image.resize((width, height))
-font1 = Font(file='assets/Nexa Bold (1).ttf', family="Nexa")
-# Convert the image to Tkinter-compatible format
-tk_image = ImageTk.PhotoImage(image)
+        self.maximized = False
 
-# Create a label and set the image
-image_label = tk.Label(window, image=tk_image)
+        # Creating fonts:
+        self.small_calibri_font = customtkinter.CTkFont(family='Calibri', size=15)
+        self.small_helvetica_font = customtkinter.CTkFont(family='Helvetica', size=12)
 
-# Pack the label to display it in the window
-image_label.grid(row=0, column=0, padx=160, pady=10, sticky="nsew")
+        self.button_font = customtkinter.CTkFont(family='Helvetica', size=25)
 
-# Create the style for the elements
-style = tk.ttk.Style()
-style.configure('TEntry', font=('Arial', 14))
-style.configure('TLabel', font=('Arial', 14))
-style.configure('TCombobox', font=('Arial', 14))
+        self.bottom_font_bold = customtkinter.CTkFont(family='Helvetica', weight='bold', size=14)
+        self.bottom_font_thin = customtkinter.CTkFont(family='Helvetica', size=12)
 
-# Create the widgets
-num_accounts_label = tk.ttk.Label(window, text="Number of Accounts", borderwidth=0)
-num_accounts_entry = tk.Entry(window, width=27, relief=tk.FLAT, bg='#F9F9F9', fg='black',
-                              insertbackground='black')
+        # Configuring window:
+        self.geometry(f"{572}x{849}")
+        self.resizable(False, False)
+        self.title(self.window_title)
+        self.overrideredirect(True)
+        self.attributes('-topmost', True, '-alpha', 0.998)
 
-api_key_label = tk.ttk.Label(window, text="CAPMONSTER API Key")
-api_key_entry = tk.ttk.Entry(window)
+        # Creating title bar:
+        self.title_bar_color = '#141414'
+        self.text_color = 'white'
+        self.hover_button_color = '#7c7d7c'
+        self.hover_close_button_color = '#ff4d4d'
 
-region_label = tk.ttk.Label(window, text="Region")
-region_combobox = ttk.Combobox(window, values=list(REGIONS.keys()))
+        self.w_width = self.winfo_width()
+        self.title_bar = customtkinter.CTkFrame(self, fg_color=self.title_bar_color, width=self.w_width, height=28,
+                                                corner_radius=0)
+        self.title_bar.grid_propagate(False)
+        self.title_bar.grid(row=0, column=0, sticky='w')
+        self.title_bar.columnconfigure(0, weight=1)
 
-# Configure the style for the generate button
+        self.s_width = self.winfo_screenwidth()
+        self.s_height = self.winfo_screenheight()
+        self.title_bar_height = self.title_bar.winfo_height()
+        self.main_frame_maximized_height = self.s_height - self.title_bar_height
 
-generate_button = tk.Button(window,
-                            background="#CC2828",
-                            foreground='white',
-                            activebackground='#8E1E1E',
-                            activeforeground='white',
-                            highlightthickness=2,
-                            highlightcolor='white',
-                            width=23,
-                            height=2,
+        def minimize(hide=False):
+            hwnd = windll.user32.GetParent(self.winfo_id())
+            windll.user32.ShowWindow(hwnd, 0 if hide else 6)
 
-                            text="Generate Accounts",
-                            command=generate_accounts_in_thread)
+        # Creating buttons, label and icon on title bar:
+        self.minimize_button = customtkinter.CTkButton(self.title_bar, text='  🗕  ', command=minimize,
+                                                       font=self.small_calibri_font, height=28, width=45,
+                                                       corner_radius=0, fg_color=self.title_bar_color,
+                                                       hover_color=self.hover_button_color, text_color=self.text_color)
+        self.minimize_button.grid(row=0, column=1, sticky='w')
 
-# Create the accounts treeview
-account_treeview = ttk.Treeview(window, columns=('Username', 'Password'), show='headings')
-account_treeview.heading('Username', text='Username')
-account_treeview.heading('Password', text='Password')
+        self.close_button = customtkinter.CTkButton(self.title_bar, text='  🗙  ', command=self.destroy,
+                                                    font=self.small_calibri_font, height=28, width=46, corner_radius=0,
+                                                    fg_color=self.title_bar_color,
+                                                    hover_color=self.hover_close_button_color,
+                                                    text_color=self.text_color)
+        self.close_button.grid(row=0, column=3, sticky='e')
 
-save_button = tk.Button(window,
-                        background="#CC2828",
-                        foreground='white',
-                        activebackground='#8E1E1E',
-                        activeforeground='white',
-                        highlightthickness=2,
-                        highlightcolor='white',
-                        width=23,
-                        height=2,
-                        text="Save Accounts",
-                        command=save_accounts,
-                        )
+        self.title_bar_title = customtkinter.CTkLabel(self.title_bar, text=self.window_title,
+                                                      fg_color=self.title_bar_color, font=self.small_helvetica_font)
+        self.title_bar_title.grid(row=0, column=0, sticky='w', padx=5)
 
-# Add the widgets to the window
-num_accounts_label.grid(row=2, column=0, padx=50, pady=10, sticky="w")
-num_accounts_entry.grid(row=2, column=0, padx=90, pady=10, ipady=5, sticky="e")
+        if self.put_icon == True:
+            self.app_icon = customtkinter.CTkImage(dark_image=Image.open(resource_path("assets/rglogo.ico")),
+                                                   size=(17, 17))
+            self.title_bar_title.configure(image=self.app_icon, compound='left', text=(f' {self.window_title}'))
 
-# api_key_label.pack(pady=10)
-# api_key_entry.pack(pady=5)
-# region_label.pack(pady=10)
-# region_combobox.pack(pady=5)
-# generate_button.pack(pady=20)
-# account_treeview.pack(pady=10)
-# save_button.pack(pady=10)
+        self.main_frame = tk.Frame(self, bg="#141414", width=572, height=849)
+        self.main_frame.place(x=0, y=28)
 
-# Run the event loop
-window.mainloop()
+        # Open the image
+        image = Image.open(resource_path("assets/rglogo.png"))
+        # Resize the image
+        new_width = 212
+        new_height = 212
+        resized_image = image.resize((new_width, new_height))
+
+        # Convert the resized image to a PhotoImage object
+        photo_image = ImageTk.PhotoImage(resized_image)
+
+        # Create a label widget and place it at the desired position
+        image_label = customtkinter.CTkLabel(self, image=photo_image, text="", bg_color='#141414')
+        image_label.place(x=180, y=62)
+
+        # Creating main frame and your main app:
+        self.information_frame = tk.Frame(self, bg="#1B1B1B", width=475, height=130)
+        self.information_frame.place(x=48, y=319)
+
+        # Number of accounts
+
+        image_label = customtkinter.CTkLabel(self,
+                                             image=ImageTk.PhotoImage(
+                                                 Image.open(resource_path("assets/user3.png")).resize((28, 28))),
+                                             text="", bg_color='#1B1B1B')
+        image_label.place(x=59, y=329)
+
+        customtkinter.CTkLabel(self, text='Number of account', font=self.small_helvetica_font, bg_color="#1B1B1B",
+                               text_color='#7F7F7F').place(
+            x=95, y=331)
+
+        self.nof_entry = customtkinter.CTkEntry(self, bg_color='#1B1B1B', height=22, width=233, corner_radius=8)
+        self.nof_entry.place(x=227, y=332)
+
+        # CapMonster Key
+        customtkinter.CTkLabel(self,
+                               image=ImageTk.PhotoImage(Image.open(resource_path("assets/key1.png")).resize((28, 28))),
+                               text="", bg_color='#1B1B1B').place(x=54, y=372)
+
+        customtkinter.CTkLabel(self, text='CapMonster Key', bg_color="#1B1B1B", text_color='#7F7F7F').place(
+            x=95, y=375)
+
+        self.CapMonster = customtkinter.CTkEntry(self, bg_color='#1B1B1B', height=22, width=233, corner_radius=8)
+        self.CapMonster.place(x=227, y=377)
+
+        # Region
+
+        customtkinter.CTkLabel(self,
+                               image=ImageTk.PhotoImage(
+                                   Image.open(resource_path("assets/countries.png")).resize((28, 28))),
+                               text="", bg_color='#1B1B1B').place(x=56, y=419)
+
+        customtkinter.CTkLabel(self, text='Region', bg_color="#1B1B1B", text_color='#7F7F7F').place(
+            x=95, y=417)
+
+        combobox_var = customtkinter.StringVar(value="EUW")
+        self.Region = customtkinter.CTkComboBox(master=self,
+                                                values=["EUW", "EUNE", 'NA', 'BR', 'TR', 'RU', 'OCE', 'LAN', 'LAS',
+                                                        'JP',
+                                                        'PH', 'SG', 'TH', 'TW', 'PBE'],
+                                                variable=combobox_var)
+        self.Region.place(x=227, y=415)
+
+        self.create_button = customtkinter.CTkButton(self, text='Create', width=221, height=66, text_color='#E2E2E2',
+                                                     fg_color='#CC2828', font=self.button_font, hover_color='#dd5555',
+                                                     command=self.generate_accounts_in_thread)
+        self.create_button.place(x=175, y=565)
+
+        # Creating bottom frame frame
+        self.bottom_frame = tk.Frame(self, bg="#101010", width=583, height=177)
+        self.bottom_frame.place(x=0, y=672)
+
+        # ------------------------------------------------
+        # Open the image
+        image = Image.open(resource_path("assets/rglogo.png"))
+        # Resize the image
+        new_width = 80
+        new_height = 80
+        resized_image = image.resize((new_width, new_height))
+
+        # Convert the resized image to a PhotoImage object
+        photo_image = ImageTk.PhotoImage(resized_image)
+
+        # Create a label widget and place it at the desired position
+        customtkinter.CTkLabel(self, image=photo_image, text="", bg_color='#101010').place(x=19, y=717)
+
+        # support and privacy
+        customtkinter.CTkLabel(self, text='Support and Privacy', font=self.bottom_font_bold, text_color='#282727',
+                               bg_color='#101010').place(x=149, y=694)
+
+        # Help Center
+        customtkinter.CTkLabel(self, text='Help Center', font=self.bottom_font_thin, text_color='#282727',
+                               bg_color='#101010').place(x=149, y=716)
+
+        # TOS
+        customtkinter.CTkLabel(self, text='Terms of Use', font=self.bottom_font_thin, text_color='#282727',
+                               bg_color='#101010').place(x=149, y=738)
+
+        # Privacy Policy
+        customtkinter.CTkLabel(self, text='Privacy Policy', font=self.bottom_font_thin, text_color='#282727',
+                               bg_color='#101010').place(x=149, y=760)
+
+        # ------------------------------------------------
+        # Social
+        customtkinter.CTkLabel(self, text='Social', font=self.bottom_font_bold, text_color='#282727',
+                               bg_color='#101010').place(x=354, y=694)
+
+        # Discord
+        customtkinter.CTkLabel(self, text='Discord', font=self.bottom_font_thin, text_color='#282727',
+                               bg_color='#101010').place(x=354, y=720)
+
+        # Youtube
+        customtkinter.CTkLabel(self, text='Youtube', font=self.bottom_font_thin, text_color='#282727',
+                               bg_color='#101010').place(x=354, y=741)
+
+        # ------------------------------------------------
+
+        # ――――――――――
+
+        def get_pos(event):
+            xwin = self.winfo_x()
+            ywin = self.winfo_y()
+            startx = event.x_root
+            starty = event.y_root
+
+            ywin = ywin - starty
+            xwin = xwin - startx
+
+            def move_window(event):
+                self.geometry(f'+{event.x_root + xwin}+{event.y_root + ywin}')
+
+            def release_window(event):
+                self.config(cursor="arrow")
+
+            self.title_bar.bind('<B1-Motion>', move_window)
+            self.title_bar.bind('<ButtonRelease-1>', release_window)
+            self.title_bar_title.bind('<B1-Motion>', move_window)
+            self.title_bar_title.bind('<ButtonRelease-1>', release_window)
+
+        self.title_bar.bind('<Button-1>', get_pos)
+        self.title_bar_title.bind('<Button-1>', get_pos)
+
+        def set_appwindow(mainWindow):
+            GWL_EXSTYLE = -20
+            WS_EX_APPWINDOW = 0x00040000
+            WS_EX_TOOLWINDOW = 0x00000080
+
+            hwnd = windll.user32.GetParent(mainWindow.winfo_id())
+            stylew = windll.user32.GetWindowLongW(hwnd, GWL_EXSTYLE)
+            stylew = stylew & ~WS_EX_TOOLWINDOW
+            stylew = stylew | WS_EX_APPWINDOW
+            res = windll.user32.SetWindowLongW(hwnd, GWL_EXSTYLE, stylew)
+
+            mainWindow.wm_withdraw()
+            mainWindow.after(10, lambda: mainWindow.wm_deiconify())
+
+        set_appwindow(self)
+
+    def generate_account(self):
+        num_accounts = self.nof_entry.get()
+
+        accounts = []
+        if not num_accounts.isdigit() or int(num_accounts) <= 0:
+            CTkMessagebox(title="Invalid Input",
+                          message="Please enter a valid positive integer for the number of accounts.", icon="cancel")
+
+            return
+
+        num_accounts = int(num_accounts)
+
+        try:
+            account_generator = GenerateAccount()
+            account_generator.CAPMONSTER_KEY = self.CapMonster.get()  # Set the CAPMONSTER_KEY
+
+            for _ in range(num_accounts):
+                self.create_button.configure(text=f'Generating ({_} / {num_accounts})', state='disabled')
+                password = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+                username = ''.join(random.choices(string.ascii_lowercase, k=8))
+                Region = self.Region.get()
+                response = account_generator.generate_account(password, username, Region)
+
+                accounts.append(f"Username: {username}, Password: {password}, Region: {Region}")
+
+            # Save the accounts to a text file
+            file_name = f"{num_accounts}x{Region} {random.randint(1000, 12345)}.txt"
+            with open(file_name, "w") as file:
+                file.write("\n".join(accounts))
+
+            CTkMessagebox(title="Accounts Saved", message=f"Accounts saved to {file_name}", icon="info")
+
+            self.create_button.configure(text=f'Create', state='normal')
+        except Exception as e:
+
+            CTkMessagebox(title="Error", message=f"An error occurred: {str(e)}", icon="cancel")
+
+    def generate_accounts_in_thread(self):
+        num_accounts = self.nof_entry.get()
+
+        if num_accounts.isdigit() and int(num_accounts) > 0:
+            thread = threading.Thread(target=self.generate_account)
+            thread.start()
+
+        else:
+
+            CTkMessagebox(title="Invalid Input",
+                          message="Please enter a valid positive integer for the number of accounts.", icon="cancel",
+                          bg_color='#141414', fg_color='#1D1D1D')
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
